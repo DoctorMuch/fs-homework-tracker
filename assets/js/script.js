4ce9815133694b115e0645940f0709923bf7de6c
Doctor Mud <davidmueth@gmail.com>
2021-09-20 19:27:19 -0500

    changing branches

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+const searchFormEl = document.getElementById("search-form");
+const searchEl = document.getElementById("start");
+const input = searchEl.value.trim();
+
+
+let geoKey = "&apiKey=af0eec2b25d34eb690dca8d75d93b100";
+let geoApiUrlRoot = "https://api.geoapify.com/v1/geocode/search?text=";
+
+let searchHandler = function(event){
+  event.preventDefault();
+  testFetch(input);
+  console.log(input);
+}
+let testFetch = function(city){
+  fetch("https://api.geoapify.com/v1/geocode/search?text=london&limit=3&type=city&filter=countrycode:us,mo"+geoKey)
+  .then(function(res){
+    res.json()
+    .then(function(info){
+      console.log(info.features[0].geometry.coordinates);
+      return info.features[0].geometry.coordinates[0];
+    })
+  })
+}
+
+searchFormEl.addEventListener("submit",searchHandler);
