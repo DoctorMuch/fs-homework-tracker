079ba1becb6dede942a89a9960891087fd991d38
Doctor Mud <davidmueth@gmail.com>
2021-09-22 15:58:22 -0500

    pushing with limited functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,6 @@ const zipInput = searchEl?.value?.trim();
 
 
 let geoApiUrlRoot = "https://api.geoapify.com/v1/geocode/search?text=";
-const headers = new Headers();
-
-headers.append("Authorization", `Bearer ${yelpKey}`);
-headers.append("Access-Control-Allow-Origin", "*");
 
 // proxy
 const proxyUrl = "https://cors-anywhere.herokuapp.com/";
@@ -15,6 +11,11 @@ const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 // Sample input is St. Louis. We can replace with our input eventually.
 const sampleInput = "&latitude=39.819382402&longitude=-89.645660649";
 const yelpApiUrl = `https://api.yelp.com/v3/businesses/search?term=coffee${sampleInput}`;
+
+
+const headers = new Headers();
+headers.append("Authorization", `Bearer ${yelpKey}`);
+headers.append("Access-Control-Allow-Origin", "*");
 
 const requestOptions = {
   method: "GET",
@@ -30,7 +31,12 @@ fetch(`${proxyUrl}${yelpApiUrl}`, requestOptions)
     const coffeePlaces = result.businesses;
     console.log("coffeePlaces = ", coffeePlaces);
     $(".card-body").append(
-      `<h1>${coffeePlaces[0].name}: ${coffeePlaces[0].rating} stars</h1>`
+      `<h1>${coffeePlaces[0].name}</h1>
+       <h2>${coffeePlaces[0].rating} stars</h2>
+        <address>
+        ${coffeePlaces[0].location.display_address[0]}
+        ${coffeePlaces[0].location.display_address[1]}
+        </address>`
     );
   })
   .catch((error) => console.log("error", error));
@@ -53,24 +59,6 @@ let coordFetch = function(zipcode){
   })
 };
 
-
-// Failed YelpFusionApi call attempt(s)
-// let myHeaders = new Headers();
-// myHeaders.append("Host", "api.yelp.com");
-// myHeaders.append("Authorization", "Bearer AxElVNzLgKgxOjwHwRQhVWwefF8-zwSvByZ-veYfEtaZ53PaGII5hNE2s2W5BKjOhVCVycya1xmUDUGTffG3h8qA16VAzuWpCF5MqprKT9se4bbKgOiYaUn3X5FKYXYx");
-// myHeaders.append("Access-Control-Allow-Origin", "same-origin");
-// myHeaders.append("Cache-Control", "no-cache");
-
-// let requestOptions = {
-//   method: 'GET',
-//   headers: myHeaders, 
-//   redirect: 'follow'
-// };
-
-// fetch("https://api.yelp.com/v3/businesses/search?location=new+york", requestOptions)
-// .then(response => response.text())
-// .then(result => console.log(result))
-// .catch(error => console.log('error', error));
  
 let showResults = function(lat, lon){
   let resultsEl = document.getElementById("results");
